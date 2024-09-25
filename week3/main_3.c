93d0c02df1952d6383a24d458d69c2200c0fe1a5
e2zi22 <ye1387@naver.com>
2024-09-25 14:34:20 +0900

    week3_yerin

---
+++ week3/main_3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char c;
+	int n;
+	
+	
+	printf("enter a character: ");
+	scanf("%c", &c);
+	
+	char n_c = c + 1;
+	
+	
+	printf("the next chracter of %c (%d)) is %c (%d)", c, c, n_c, n_c);
+	
+	
+	return 0;
+}
